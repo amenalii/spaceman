80822dc5c0742d1d491191aaeadcf5f048ce4e37
amenalii <ummeamen@gmail.com>
2024-12-11 18:01:56 -0600

    Adding CSS

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
+// 1) Define any variables used to track the state of the game:
+//    The players choice
+//    The match result - win/lose
+//    A result message - display who won that round
+
+// 2) Define the required constants:
+
+// 3) Handle a player clicking a button
+
+// 4) Handle generating random selections for the word
+
+// 5) Compare the player choice to the selected word
+
+// 6) Render a win/lose message to the player
+
+
 /*-------------------------------- Constants --------------------------------*/
 const words = [
     "supernova", "asteroid", "galaxy", "orbit", "spacesuit", "blackhole",
@@ -7,10 +23,13 @@ const words = [
     "phenomenon", "satellite", "astronaut"
 ];
 
-const buttons = document.querySelectorAll('.key');
 /*-------------------------------- Variables --------------------------------*/
+let playerChoice;
+let msg;
 
 /*------------------------ Cached Element References ------------------------*/
+const buttons = document.querySelectorAll('.key');
+const resetbutton = document.querySelector('.reset')
 
 /*----------------------------- Event Listeners -----------------------------*/
 buttons.forEach(button => {
@@ -18,5 +37,9 @@ buttons.forEach(button => {
         const key = event.target.textContent;
         console.log(key)
     })
+})
+
+resetbutton.addEventListener('click', (event) => {
+    console.log(event.target.textContent)
 })
 /*-------------------------------- Functions --------------------------------*/
